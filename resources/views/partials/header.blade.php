ba338ebea14cf0cdefb44c438e5b52cc2a5d1a49
LucaBrivio183 <l.brivio4@gmail.com>
2023-05-02 16:20:58 +0200

    feat: home layouts, styles partial, comics added

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,13 @@
+<header>
+    <div class="container">
+        <div class="row">
+            <a href="">DC POWER VISA</a>
+            <a href="">ADDITIONAL DC SITES</a> {{-- dropdown menu --}}
+        </div>
+    </div>
+    <div class="row">
+        <div class="logo-box">
+            <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="">
+        </div>      
+    </div>
+</header>
