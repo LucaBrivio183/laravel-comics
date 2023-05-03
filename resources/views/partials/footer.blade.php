8806f6ddde0b438c7b5e059bbf347bbdd32f1100
LucaBrivio183 <l.brivio4@gmail.com>
2023-05-03 18:44:10 +0200

    feat: footer style

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,12 +1,61 @@
 <footer>
-    <div class="container">
-        <div class="footer-bottom d-flex align-items-center justify-content-between">
-            <span>FOLLOW US</span>
-            <ul class="d-flex align-items-center">
-                @foreach ($socials as $item)
-                <li><a href="{{$item['link']}}"><img :src="{{$item['logo']}}" alt="{{$item['name']}}"></a></li>
-                @endforeach  
-            </ul>
+    <div class="footer-top p-4">
+        <div class="container">
+            <div class="row w-50">
+                <div class="col d-flex flex-column">
+                    <h3 class="py-2">DC COMICS</h3>
+                    <a class="p-1" href="">Characters</a>
+                    <a class="p-1" href="">comics</a>
+                    <a class="p-1" href="">Movies</a>
+                    <a class="p-1" href="">TV</a>
+                    <a class="p-1" href="">Games</a>
+                    <a class="p-1" href="">Videos</a>
+                    <a class="p-1" href="">News</a>
+                    <h3 class="py-2">SHOP</h3>
+                    <a class="p-1" href="">Shop DC</a>
+                    <a class="p-1" href="">Shop DC Collectibles</a>
+                </div>
+                <div class="col d-flex flex-column">
+                    <h3 class="py-2">DC</h3>
+                    <a class="p-1" href="">Terms Of Use</a>
+                    <a class="p-1" href="">Privacy policy (New)</a>
+                    <a class="p-1" href="">Ad Choices</a>
+                    <a class="p-1" href="">Advertising</a>
+                    <a class="p-1" href="">Jobs</a>
+                    <a class="p-1" href="">Subscriptions</a>
+                    <a class="p-1" href="">Talent Workshops</a>
+                    <a class="p-1" href="">CPSC Certificates</a>
+                    <a class="p-1" href="">Ratings</a>
+                    <a class="p-1" href="">Shop Help</a>
+                    <a class="p-1" href="">Contact Us</a>
+                </div>
+                <div class="col d-flex flex-column">
+                    <h3 class="py-2">SITES</h3>
+                    <a class="p-1" href="">DC</a>
+                    <a class="p-1" href="">MAD Magazine</a>
+                    <a class="p-1" href="">DC Kids</a>
+                    <a class="p-1" href="">DC Universe</a>
+                    <a class="p-1" href="">DC Power Visa</a>
+                </div>
+            </div>
+            <img class="w-50" src="{{ Vite::asset('resources/img/dc-logo-bg.png')}}" alt="">
+        </div>
+    </div>
+    <div class="footer-bottom p-2 ">
+        <div class="container">
+            <div class="footer-bottom d-flex align-items-center justify-content-between">
+                <a href="" class="btn btn-outline-primary">SIGN UP NOW!</a>
+                <div class="socials d-flex align-items-center justify-content-between">
+                    <span class="text-center">FOLLOW US</span>
+                    <ul class="d-flex align-items-center">
+                        <li class="p-2"><a href="#"><img src="{{ Vite::asset('resources/img/footer-facebook.png') }}" alt=""></a></li>
+                        <li class="p-2"><a href="#"><img src="{{ Vite::asset('resources/img/footer-twitter.png') }}" alt=""></a></li> 
+                        <li class="p-2"><a href="#"><img src="{{ Vite::asset('resources/img/footer-youtube.png') }}" alt=""></a></li>
+                        <li class="p-2"><a href="#"><img src="{{ Vite::asset('resources/img/footer-pinterest.png') }}" alt=""></a></li>
+                        <li class="p-2"><a href="#"><img src="{{ Vite::asset('resources/img/footer-periscope.png') }}" alt=""></a></li>
+                    </ul>
+                </div>
+            </div>
         </div>
     </div>
 </footer>
